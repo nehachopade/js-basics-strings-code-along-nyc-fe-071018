1e6fa10c0e64e709651d700374ce8e25b336e062
Neha Chopade <nehachopade22@gmail.com>
2018-08-14 02:32:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ let excitedWelcomeMessage='WELCOME TO FLATBOOK, '+ upperCaseCurrentUser +"!";
 
 const firstInitial = currentUser[0];
 const restOfName = currentUser.slice(1);
-let shortGreeting="Welcome, "+ firstInitial;
+let shortGreeting="Welcome, "+ firstInitial +"!";
