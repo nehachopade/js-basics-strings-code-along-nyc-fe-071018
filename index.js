67bdc7c76d4dbac922aa2eafff631937bf3c110e
Neha Chopade <nehachopade22@gmail.com>
2018-08-14 02:27:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser="Neha Chopade";
+let welcomeMessage='Welcome to Flatbook, '+ currentUser + '!';
+
+const upperCaseCurrentUser = currentUser.toUpperCase();
+let excitedWelcomeMessage='WELCOME TO FLATBOOK, '+ upperCaseCurrentUser +"!";
