a2a3e3921d374f8b9d0903eb30358d08d0e5f7cd
Neha Chopade <nehachopade22@gmail.com>
2018-08-14 02:30:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ let welcomeMessage='Welcome to Flatbook, '+ currentUser + '!';
 const upperCaseCurrentUser = currentUser.toUpperCase();
 let excitedWelcomeMessage='WELCOME TO FLATBOOK, '+ upperCaseCurrentUser +"!";
 
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+expect(shortGreeting).to.have.string(firstInitial);
+expect(shortGreeting).to.not.have.string(restOfName);
 let shortGreeting="Welcome, "+
