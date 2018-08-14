fbf4a9edc02f4c2b2283d357ee301889ef7aac10
Neha Chopade <nehachopade22@gmail.com>
2018-08-14 02:33:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser="Neha Chopade";
+let welcomeMessage='Welcome to Flatbook, '+ currentUser + '!';
+
+const upperCaseCurrentUser = currentUser.toUpperCase();
+let excitedWelcomeMessage='WELCOME TO FLATBOOK, '+ upperCaseCurrentUser +"!";
+
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+let shortGreeting="Welcome, "+ firstInitial +"!";
